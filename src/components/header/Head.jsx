ff65d3d82fc7011efb1bc7eb510cc718943d15cf
Bhaskar Mandal <bittumyt927@gmail.com>
2025-05-05 23:08:14 +0530

    few changes

--- src/components/header/Head.jsx
+++ src/components/header/Head.jsx
@@ -1,38 +1,33 @@
 import React, { useState } from "react";
-import { GoLaw } from "react-icons/go";
-import { IoIosMenu } from "react-icons/io";
 import "./head.css";
-import { SideBar } from "../sidebar/SideBar";
-import { useNavigate } from "react-router-dom";
-export const Head = () => {
-  const [menu, setMenu] = useState("initial");
-  let location = useNavigate();
-  const toggle = () => {
-    if (menu == "initial" || menu == "close") {
-      setMenu("open");
-    } else {
-      setMenu("close");
-    }
-  };
-  return (
-    <div className="head">
-      <SideBar menu={menu} toggle={toggle} />
-      <div className="one" onClick={() => location("/")}>
-        <GoLaw size={20} />
-        <span>nn ic info</span>
-      </div>
+import { FaBalanceScale } from "react-icons/fa"; // Importing an icon for the logo
+import { SideBar } from "../sidebar/SideBar"; // Importing the SideBar component
 
-      <div className="two">
-        <div className="two-one">Home</div>
-        <div className="two-two">About</div>
-        <div className="two-three">Contact</div>
-        <div className="two-four">Blog</div>
-      </div>
-      <div className="three">
-        <button className="three-one">Login</button>
-        <button className="three-two">Sign Up</button>
-        <IoIosMenu size={25} onClick={toggle} className="line" />
-      </div>
-    </div>
+export const Head = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <header className="head">
+        <div className="head-left">
+          <FaBalanceScale className="logo-icon" />
+          <span className="logo-text">NN IC INFO</span>
+        </div>
+        <div className="head-right">
+          <button className="login-button">Login</button>
+          <button className="signup-button">Sign Up</button>
+          <div className="menu-icon" onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </header>
+      <SideBar menu={menuOpen ? "open" : "closed"} toggle={toggleMenu} />
+    </>
   );
 };
